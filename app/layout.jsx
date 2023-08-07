a9c3fd7538aadefde8af933317755c73d99bd186
jourdancatarina <jourdancatarina3@gmail.com>
2023-08-07 15:05:16 +0800

    Delete footer file

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,6 @@
 import Header from '@/components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
-import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
